598eced2d6e2b1b483353e8d415bcdc3fa067da5
Vijay Tembugade <58331578+vijaytembugade@users.noreply.github.com>
2024-05-01 12:33:22 +0000

    intro to arrays

---
+++ 01_Arrays/01_intro.js
@@ -0,0 +1,7 @@
+// Shut up buddy, why are you here if you dont know Arrays.
+
+// 1. go and learn basic of programming what exactly array is and how to acces the elemt in arrays.
+// 2. understand basic for loop and while loop
+// 3. understand the how array works in JS
+// 4. Understand for loop, forEach, map filter, reduce ,fo...in and for..of loop first.
+
