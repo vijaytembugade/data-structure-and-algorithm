fd830faf622167016ed3ec2d8746a86828aae9a6
Vijay Tembugade <58331578+vijaytembugade@users.noreply.github.com>
2024-05-14 00:22:24 +0530

    node and Tc of LL

---
+++ 04_Linked_List/01_Notes.md
@@ -0,0 +1,13 @@
+### Comparing arrays with LinkedList
+- LinkedList dont have index as array
+- Arrays are continues spaced in memory but linked list are randomly allocated in memory.
+
+### LinkedList specs
+LinkedList have `head` and `tail`
+head : points towards the first time
+tail: points towards the last time
+Each Item in linkedList points to the next item
+
+### Visual Represetation of LL
+![alt text](image-1.png)
+
---
+++ 04_Linked_List/03_Node.js
@@ -0,0 +1,58 @@
+/**
+ * what a Node made up of in linked List?
+ * -> Node is made of value and pointer to next value
+ */
+
+// Node is basically object which has a properties of value and pointer
+
+const node1 = {
+    value: 1,
+    next: null
+}
+
+// Another node
+const node2 = {
+    value: 2,
+    next: null
+}
+
+// more Another node 
+const node3 = {
+    value: 3,
+    next: null
+}
+
+/**
+ * Above three nodes are individual node but what is we connect the node and assign the next of each node to the next node
+ * node1-> node2-> node3
+ */
+
+const listOfNodes = {
+    head: {
+        //this is node1 
+        value: 1,
+        next: {
+            // this is node2
+            value: 2,
+            next: {
+                // this is node3
+                value : 3, 
+                next: tailNode // -----> tail pointer
+            },
+        }
+    }
+}
+
+// In above next of each node pointing to the another node.
+// This is how the linkedlist looks like , here what we are missing is head and tail pointer 
+
+const tailNode = {
+    // this is node3
+    value: 4,
+    next: null
+    // in this object we will have tail pointer; 
+}
+
+const tail = tailNode;
+
+// This is how linkedlist looks like.
