381d9641f1dc356e961428b02ad8ebce3faca5cd
vijay-t13 <111426266+vijay-t13@users.noreply.github.com>
2025-03-08 01:00:32 +0530

    selection sort

---
+++ 05_Selection_Sort/index.js
@@ -0,0 +1,20 @@
+const arr = [45, 9, 46, 24, 52, 20, 13];
+
+let n = arr.length;
+
+console.time("start");
+for (let i = 0; i < n - 1; i++) {
+  let minimumIndex = i;
+  for (let j = i + 1; j < n; j++) {
+    if (arr[j] < arr[minimumIndex]) {
+      minimumIndex = j;
+    }
+  }
+
+  let temp = arr[i];
+  arr[i] = arr[minimumIndex];
+  arr[minimumIndex] = temp;
+}
+console.timeEnd("start");
+
+console.log(arr);
